d1444530f9891fb8c11af16cb00d07e27356ef63
to-nans <yselbsthasst@gmail.com>
2025-06-22 09:02:37 +0900

    portfolioの基本的な構成の完成

--- portfolio/src/components/HelloWorld.vue
+++ portfolio/src/components/HelloWorld.vue
@@ -5,72 +5,88 @@
   <!-- アプリのメインコンテンツ領域を定義 画面全体を黒色に指定 -->
   <v-main class="bg-black d-flex justify-center">
 
-    <v-container  max-width="1440">
+    <v-container fluid class="px-4">
       
-      <v-row >
+      <!-- <v-row >
         <v-card class="mx-auto mt-4"
           width="1080"
           height="72"
-          color="green"
+          color="green-accent-3"
           position: absolute>
         </v-card>
-      </v-row>
-
+      </v-row> -->
+      <v-app-bar app color="green-accent-3" dark>
+        <v-toolbar-title>Tech Flow</v-toolbar-title>
+        <v-spacer />
+        <v-btn text @click="scrollToSection('AboutMe')">About Me</v-btn>
+        <v-btn text @click="scrollToSection('section2')">Our Services</v-btn>
+      </v-app-bar>
       <v-row >
 
-        <v-col cols="6" offset="2" class="text-left text-h1 ">
-            Innovative<br>
-            Solutions
+        <v-col cols="12" sm="8" md="6" offset-md="2" class="text-left">
+            <div class="text-h2 text-sm-h1">Innovative<br>
+            Solutions</div>
         </v-col>
 
       </v-row>
       
       <v-row >
-        <v-col  cols="6" offset="4" class="text-left text-h1">
-            for  Modern
+        <v-col cols="12" sm="8" md="6" offset-sm="2" offset-md="4" class="text-left">
+            <div class="text-h2 text-sm-h1">for Modern</div>
         </v-col>
       </v-row>
 
       <v-row >
-        <v-col  cols="6" offset="6" class="text-left text-h1">
-            Challenges
+        <v-col cols="12" sm="8" md="6" offset-sm="4" offset-md="6" class="text-left">
+            <div class="text-h2 text-sm-h1">Challenges</div>
         </v-col>
       </v-row>
 
       <v-row style="margin-top: 50px;">
-        <v-col cols="4" offset="2" class="text-left text-h3 ">
-          About Me
-          <p class="text-body-1">
+        <v-col cols="12" sm="6" md="4" offset-md="2" class="text-left" >
+          <div class="text-h4 text-sm-h3">About Me</div>
+          <p class="text-body-1" id="AboutMe">
             普通の社会人です。社会人になって久しく手を動かすことをしてなかったので
-            vuetifyの学習ついでにサイトを作ってみました。figmaでサイトをデザインして
+            vuetifyの学習ついでにサイトを作ってみました。figmaでサイトをデザインして、
             そのデザインをもとにvuetifyを利用し作成しました。
             使用している画像はすべてAIを使用して作成したものです。
-            これをベースにclineやcodexなどの生成AIにどんどん触れていきたいと思っています。
+            今回は自分の手を動かすことをメインにしてましたが、clineやcodexなどの生成AIにどんどん触れていきたいと思っています。
           </p>
+          <v-btn variant="plain" href="https://github.com/yselbs" color="green-accent-3" class="text-h3 text-left">
+            Github
+          </v-btn>
         </v-col>
-        <v-col>
-            <v-img src="../assets/self_face.jpg" max-width="300" contain></v-img>
+        <v-col cols="12" sm="6">
+            <v-img src="../assets/self_face.jpg" max-width="100%" max-height="300" contain class="mx-auto"></v-img>
         </v-col>
       </v-row>
-      
       <v-row style="margin-top: 50px;">
-        <v-col cols="3" offset="2">
-          <v-img src="../assets/gazou_ai.jpg"  max-width="300" contain></v-img>
+        <v-col cols="12" sm="8" md="6" offset-md="2" class="text-left">
+            <div id="section2" class="text-h4 text-sm-h3">
+                Our Services
+            </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        
+        <v-col cols="12" sm="6" md="4" lg="3" offset-lg="2" class="text-left">
+          
+          <v-img src="../assets/gazou_ai.jpg" max-width="100%" max-height="250" contain class="mx-auto"></v-img>
             <p class="text-body-1">
               画像処理AIの研究をしていた経験があります。具体的にはデータ拡張と呼ばれる学習データを補強する際に
               使用される技術に対する研究を行っており、論文発表の経験もあります。
               今は利用する側として話題の生成AIに興味があり、絶賛勉強中です。
             </p>
         </v-col>
-        <v-col cols="3" >
-          <v-img src="../assets/web_dev.jpg"  max-width="300" contain></v-img>
+        <v-col cols="12" sm="6" md="4" lg="3">
+          <v-img src="../assets/web_dev.jpg" max-width="100%" max-height="250" contain class="mx-auto"></v-img>
             <p class="text-body-1">
               簡単なwebサイトを作った経験があります。まだまだ勉強中で今後はアプリケーション開発にも
               挑戦したいと考えています。
             </p>
         </v-col>
-        <v-col cols="3" >
-          <v-img src="../assets/serve_dev.jpg"  max-width="300" contain></v-img>
+        <v-col cols="12" sm="6" md="4" lg="3">
+          <v-img src="../assets/serve_dev.jpg" max-width="100%" max-height="250" contain class="mx-auto"></v-img>
             <p class="text-body-1">
               アプリケーションのサーバーサイドの開発経験があります。まだまだ見習いなので
               当事者意識をもって取り組んで、一人前のエンジニアになれるよう頑張りたいと思います。
@@ -86,57 +102,17 @@
 </template>
 
 <script>
-
 export default {
   name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+  methods: {
+    scrollToSection(id) {
+      const el = document.getElementById(id)
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' }) // スムースにスクロール
+      }
+    }
+  },
 }
+
+
 </script>
